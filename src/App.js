fcf20f028b0b87c3cc8b7e51d5a7c8242794837a
Arthur Fucher <Arthur_Fucher@external.mckinsey.com>
2019-08-01 13:00:09 -0300

    adds simulation of title's height change on panel content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Body from './Body'
 
 function App() {
   const [openedPanel, setOpenedPanel] = useState("bothPanels");
+  const [panelTitleSize, changePanelTitleSize] = useState("small");
 
   return (
     <div className="App">
@@ -12,8 +13,11 @@ function App() {
         <button onClick={() => setOpenedPanel("bothPanels")}>BothPanels</button>
         <button onClick={() => setOpenedPanel("panel1")}>Panel1</button>
         <button onClick={() => setOpenedPanel("panel2")}>Panel2</button>
+        <button onClick={() => changePanelTitleSize("small")}>Small</button>
+        <button onClick={() => changePanelTitleSize("medium")}>Medium</button>
+        <button onClick={() => changePanelTitleSize("large")}>Large</button>
       </header>
-      <Body openedPanel={openedPanel}/>
+      <Body openedPanel={openedPanel} panelTitleSize={panelTitleSize}/>
     </div>
   );
 }
--- src/Body.js
+++ src/Body.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import './Body.css';
 
 
-export const Body = ({openedPanel}) => {
+export const Body = (props) => {
+    let openedPanel = props.openedPanel;
     return <main>
-        {(openedPanel === "bothPanels" || openedPanel === "panel1") && <Panel title={"Panel 1"} childLength={20}/>}
-        {(openedPanel === "bothPanels" || openedPanel === "panel2")  && <Panel title={"Panel 2"}/>}
-
+        {(openedPanel === "bothPanels" || openedPanel === "panel1") && <Panel title={"Panel 1"} childLength={20} {...props}/>}
+        {(openedPanel === "bothPanels" || openedPanel === "panel2")  && <Panel title={"Panel 2"} {...props}/>}
     </main>
 }
 
 
-const Panel = ({title, childLength=10, openedPanel}) => {
-    return <div className={"panel " + {openedPanel}}>
-        <h1 className={"panel-title"}>{title}</h1>
-        <div className={"panel-content"}>
+const Panel = ({title, childLength=10, openedPanel, panelTitleSize}) => {
+
+    const titleRef = useRef(null);
+    const [titleHeight,setTitleHeight] = useState(48);
+
+    useEffect(() => {
+        window.document.title = titleRef.current.offsetHeight;
+        setTitleHeight(titleRef.current.offsetHeight);
+    }, [titleRef, panelTitleSize])
+
+    return <div className={"panel " + openedPanel}>
+        <h1 ref={titleRef} className={"panel-title " + panelTitleSize}>{title}</h1>
+        <div className={"panel-content"} style={{height: `calc(100% - ${titleHeight}px)`}}>
             <FakeContentGenerator size={childLength} />
         </div>
     </div>
